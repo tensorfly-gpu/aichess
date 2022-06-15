91156e5d0c89f0f5d5050e2041c6c0d41b642bf1
一心炼银 <83876165+tensorfly-gpu@users.noreply.github.com>
2022-06-15 23:15:34 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ CONFIG = {
     'paddle_model_path': 'current_policy.model',      # paddle模型路径
     'pytorch_model_path': 'current_policy.pkl',   # pytorch模型路径
     'train_data_buffer_path': 'train_data_buffer.pkl',   # 数据容器的路径
-    'batch_size': 5000,  # batch_size大小，根据自己内存大小决定
+    'batch_size': 5000,  # batch_size大小，根据自己的显存大小决定
     'kl_targ': 0.02,  # kl散度控制
     'epochs' : 5,  # 每次更新的train_step数量
     'game_batch_num': 3000,  # 训练更新的次数
